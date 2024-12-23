73a4f3fd672be4cfe9ef43f6408a08f0fa4c6e63
zetsr <90092893+zetsr@users.noreply.github.com>
2024-12-23 22:24:58 +0800

    Delete js directory

--- js/api.js
+++
@@ -1,5 +0,0 @@
-// API 文档相关功能
-document.addEventListener('DOMContentLoaded', () => {
-    loadContent('api');  // 默认加载API文档
-  });
-  
--- js/home.js
+++
@@ -1,5 +0,0 @@
-// 主页相关功能
-document.addEventListener('DOMContentLoaded', () => {
-    loadContent('home');  // 默认加载主页
-  });
-  
